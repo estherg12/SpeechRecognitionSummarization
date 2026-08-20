2e228363808c05ea9ef51c554104e2aabf3868b2
esthergoncalves <estherg.goncalves12@gmail.com>
2026-08-20 16:38:11 +0200

    Transcribir audio a texto

---
+++ recognition.py
@@ -0,0 +1,23 @@
+from vosk import Model, KaldiRecognizer
+from pydub import AudioSegment
+import json
+
+FRAME_RATE = 16000 # Cualidad del audio
+CHANNELS = 1 # Mono
+
+model = Model(model_name="vosk-model-es-0.42") # Cargar el modelo de reconocimiento de voz
+
+rec = KaldiRecognizer(model, FRAME_RATE) # Crear un reconocedor de voz con el modelo y la calidad del audio
+rec.SetWords(True) 
+
+# Cargar el archivo de audio mp3 y convertirlo a mono y a la calidad de audio deseada
+mp3 = AudioSegment.from_file("HistoriaEspañaCorto.mp3", format="mp3") 
+mp3 = mp3.set_channels(CHANNELS)
+mp3 = mp3.set_frame_rate(FRAME_RATE)
+
+# Transcribir el audio a texto
+rec.AcceptWaveform(mp3.raw_data)
+result = rec.Result() 
+text = json.loads(result).get("text", "")
+print(text)
+
